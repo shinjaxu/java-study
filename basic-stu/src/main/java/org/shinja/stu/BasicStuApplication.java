767b89cb5554a6b856251ba894473c23348f40b1
shinja <shinjaxu@163.com>
2021-06-22 16:45:34 +0800

    ''

---
+++ basic-stu/src/main/java/org/shinja/stu/BasicStuApplication.java
@@ -0,0 +1,13 @@
+package org.shinja.stu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicStuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicStuApplication.class, args);
+	}
+
+}
---
+++ basic-stu/src/test/java/org/shinja/stu/BasicStuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shinja.stu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicStuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
